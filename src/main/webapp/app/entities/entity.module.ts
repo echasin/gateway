d2db8ff1edaac5a991f51bafabf06f9704d0fed3
vagrant <vagrant@vagrant.vm>
2017-05-14 05:05:21 +0000

    Fixed Main page not displaying

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,20 +1,18 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { GatewayAssetassetmbrModule } from './assetassetmbr/assetassetmbr.module';
 import { GatewayAssetModule } from './asset/asset.module';
 import { GatewayAssetrecordtypeModule } from './assetrecordtype/assetrecordtype.module';
 import { GatewayModelModule } from './model/model.module';
-import { GatewayAssetassetmbrModule } from './assetassetmbr/assetassetmbr.module';
-import { GatewayModelassetmbrModule } from './modelassetmbr/modelassetmbr.module';
 import { GatewayModelrecordtypeModule } from './modelrecordtype/modelrecordtype.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        GatewayAssetassetmbrModule,
         GatewayAssetModule,
         GatewayAssetrecordtypeModule,
         GatewayModelModule,
-        GatewayAssetassetmbrModule,
-        GatewayModelassetmbrModule,
         GatewayModelrecordtypeModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
