5e3d7c9c84167daad908b6de59147a7f37c3c767
Eric Chasiin <Eric Chasiin@MSI>
2017-05-16 10:49:30 -0400

    Added Assetassetmbrrecordtype

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,19 +1,21 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { GatewayAssetModule } from './asset/asset.module';
+import { GatewayAssetassetmbrModule } from './assetassetmbr/assetassetmbr.module';
 import { GatewayAssetrecordtypeModule } from './assetrecordtype/assetrecordtype.module';
 import { GatewayModelModule } from './model/model.module';
-import { GatewayAssetassetmbrModule } from './assetassetmbr/assetassetmbr.module';
 import { GatewayModelrecordtypeModule } from './modelrecordtype/modelrecordtype.module';
+import { GatewayAssetassetmbrrecordtypeModule } from './assetassetmbrrecordtype/assetassetmbrrecordtype.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         GatewayAssetModule,
+        GatewayAssetassetmbrModule,
         GatewayAssetrecordtypeModule,
         GatewayModelModule,
-        GatewayAssetassetmbrModule,
         GatewayModelrecordtypeModule,
+        GatewayAssetassetmbrrecordtypeModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
