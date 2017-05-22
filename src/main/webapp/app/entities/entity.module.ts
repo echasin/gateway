28748315413a05e6aff48a5a3a8e47524ed5d602
Eric Chasiin <Eric Chasiin@MSI>
2017-05-22 00:06:44 -0400

    commit 49 of 303

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,23 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { GatewayAssetassetmbrModule } from './assetassetmbr/assetassetmbr.module';
-import { GatewayAssetModule } from './asset/asset.module';
-import { GatewayAssetassetmbrModule } from './assetassetmbr/assetassetmbr.module';
-import { GatewayAssetrecordtypeModule } from './assetrecordtype/assetrecordtype.module';
-import { GatewayModelModule } from './model/model.module';
-import { GatewayModelrecordtypeModule } from './modelrecordtype/modelrecordtype.module';
-import { GatewayAssetassetmbrrecordtypeModule } from './assetassetmbrrecordtype/assetassetmbrrecordtype.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        GatewayAssetassetmbrModule,
-        GatewayAssetModule,
-        GatewayAssetassetmbrModule,
-        GatewayAssetrecordtypeModule,
-        GatewayModelModule,
-        GatewayModelrecordtypeModule,
-        GatewayAssetassetmbrrecordtypeModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
